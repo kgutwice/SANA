b596b6fa8523f6502e75479cb2d949585f46e4a0
maxpridy <maxpridy@gmail.com>
2017-11-14 18:53:02 +0900

    overall modified

--- app/src/main/java/soen/kgutwice/sana/MainActivity.java
+++ app/src/main/java/soen/kgutwice/sana/MainActivity.java
@@ -1,5 +1,6 @@
 package soen.kgutwice.sana;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -68,6 +69,8 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(getApplicationContext(), AddSchedule.class);
+            startActivity(intent);
             return true;
         }
 
