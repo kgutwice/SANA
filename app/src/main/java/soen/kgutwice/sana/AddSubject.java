c9f6dfb53ed396f3875aa29206f61bce7c7c39a5
maxpridy <maxpridy@gmail.com>
2017-11-14 19:36:18 +0900

    Modify activity name schedule to subject

--- app/src/main/java/soen/kgutwice/sana/AddSubject.java
+++ app/src/main/java/soen/kgutwice/sana/AddSubject.java
@@ -8,6 +8,6 @@ public class AddSubject extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_schedule);
+        setContentView(R.layout.activity_add_subject);
     }
 }
