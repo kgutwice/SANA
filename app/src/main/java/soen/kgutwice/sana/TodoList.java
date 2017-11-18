b560f4e35e15d45693359f6c80a29f9c30c0e23c
maxpridy <maxpridy@gmail.com>
2017-11-18 20:32:56 +0900

    modified Todolist

--- app/src/main/java/soen/kgutwice/sana/TodoList.java
+++ app/src/main/java/soen/kgutwice/sana/TodoList.java
@@ -1,7 +1,13 @@
 package soen.kgutwice.sana;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class TodoList extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class TodoList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
+
+        String[] items = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"};
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        ListView listView = (ListView)findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int i, long id){
+                        Intent intent = new Intent(getApplicationContext(), ModifyTodo.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
